289f8544193abe36b49a2505967b1d44b5a8a68a
Mateus FAustino <mateusfaustino2017@gmail.com>
2024-04-08 22:04:44 -0300

    feat: uso do Try Catch

--- script.js
+++ script.js
@@ -16,23 +16,34 @@ function activeModal(){
 }
 
 function salvarTarefa(event){
-    event.preventDefault()
-    let $listaTarefa = document.querySelector("#listaTarefa")
-    let $task = document.querySelector("#task")
-    let tarefaAtual = $task.value
-    $listaTarefa.innerHTML+=`
-    <div class="itemtarefa" id="tarefa-${contador}">
-        <div class="form-check">
-            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+    try {
+        event.preventDefault()
+        let $listaTarefa = document.querySelector("#listaTarefa")
+        let $task = document.querySelector("#task")
+        let tarefaAtual = $task.value
+        if(tarefaAtual==''){
+            throw 'O campo não pode estar vazio'
+        }
+        let $errorMessage = document.getElementById("error-message")
+        $errorMessage.innerText = ''
+        $listaTarefa.innerHTML+=`
+        <div class="itemtarefa" id="tarefa-${contador}">
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+            </div>
+            <h3>
+                ${tarefaAtual}
+            </h3>
+            <button type="button" class="btn btn-danger" onclick="deletarTarefa('${'tarefa-'+contador}')">Excluir</button>
         </div>
-        <h3>
-            ${tarefaAtual}
-        </h3>
-        <button type="button" class="btn btn-danger" onclick="deletarTarefa('${'tarefa-'+contador}')">Excluir</button>
-    </div>
-    `
-    contador++
-    console.log(contador)
+        `
+        contador++
+        console.log(contador)
+    } catch (error) {
+        let $errorMessage = document.getElementById("error-message")
+        $errorMessage.innerText = error
+    }
+    
 } 
 
 function cancelar(event){
